46fcdfc191f64a6d2d1593fb89cd93b75dad0402
Roshni <1712roshni@gmail.com>
2022-01-08 18:47:03 +0530

    2nd commit

--- app/src/main/java/com/example/asjProject/Recycler.kt
+++ app/src/main/java/com/example/asjProject/Recycler.kt
@@ -4,6 +4,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
 
 class Recycler(): RecyclerView.Adapter<Recycler.ViewHolder>() {
@@ -22,9 +24,23 @@ class Recycler(): RecyclerView.Adapter<Recycler.ViewHolder>() {
         holder.itemImage.setImageResource(images[position])
     }
 
-      class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
     {
         var itemImage: ImageView = itemView.findViewById(R.id.image_1)
         var itemTitle: TextView  =itemView.findViewById(R.id.text_title)
+        val intent: Intent? = null
+        init{
+        itemView.setOnClickListener{
+            val position: Int =adapterPosition
+
+            Toast.makeText(itemView.context,"you clicked on ${title[position]}", Toast.LENGTH_LONG).show()
+            if(position==0)
+            itemView.context.startActivity(Intent(itemView.context, Activity2::class.java))
+            if(position==1)
+                itemView.context.startActivity(Intent(itemView.context, Activity3::class.java))
+            if(position==2)
+                itemView.context.startActivity(Intent(itemView.context, Activity4::class.java))}}
+
+        }
     }
-}
+    }
